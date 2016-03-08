3b870a97cc5e130c10b78e7c3297574f2c3d7be2
hzhang <Haijun.Zhang@kisters.cn>
2016-03-08 14:38:32 +0800

    {nocase} play music from npr

--- js/main.js
+++ js/main.js
@@ -1,36 +1,45 @@
 var app = angular.module('myApp', []);
-app.run(function($rootScope) {
-  $rootScope.name = "Ari Lerner";
-});
 
-app.controller('MyController', function($scope) {
-  $scope.person = {
-    name: ""
-  };
-});
-
-app.controller('MyController', function($scope) {
-  $scope.person = { name: "Ari Lerner" };
-  var updateClock = function() {
-    $scope.time = new Date();
-  };
-  var timer = setInterval(function() {
-    $scope.$apply(updateClock);
-  }, 1000);
-  updateClock();
-});
-
-app.controller('DemoController', function($scope) {
-  $scope.counter = 0;
-  $scope.add = function(amount) { $scope.counter += amount; };
-  $scope.subtract = function(amount) { $scope.counter -= amount; };
-});
 
 var apiKey = 'MDIzMDk0NzE0MDE0NTc0MTQ0OTcwYzk3Mg000',
     nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
  
+
+app.directive('nprLink', function() {
+   return {
+     restrict: 'EA',
+     require: ['^ngModel'],
+     replace: true,
+     scope: {
+       ngModel: '=',
+       play: '&'
+     },
+     templateUrl: '/views/nprListItem.html',
+     link: function(scope, ele, attr) {
+       scope.duration = scope.ngModel.audio[0].duration.$text;
+     }
+   } 
+});
+
+
 app.controller('PlayerController', function($scope, $http) {
   
+var audio = document.createElement('audio');
+  $scope.audio = audio;
+
+  // audio.src = 'http://pd.npr.org/npr-mp4/npr/sf/2013/07/20130726_sf_05.mp4?orgId=1&topicId=1032&ft=3&f=61';
+  // audio.play();
+
+  $scope.play = function(program) {
+    if ($scope.playing) audio.pause();
+    var url = program.audio[0].format.mp4.$text;
+    audio.src = url;
+    audio.play();
+    // Store the state of the player as playing
+    $scope.playing = true;
+  }
+
+
 // Hidden our previous section's content
   
 // construct our http request
@@ -39,6 +48,7 @@ app.controller('PlayerController', function($scope, $http) {
     url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
   }).success(function(data, status) {
     $scope.programs = data.list.story;
+
 // Now we have a list of the stories (data.list.story)
     
 // in the data object that the NPR API 
@@ -58,4 +68,10 @@ app.controller('PlayerController', function($scope, $http) {
     
 // Some error occurred
   });
+
+  
+	
 });
+
+
+
