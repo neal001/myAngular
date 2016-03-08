1e26c4da7e06b771a5c1b727a9e3b3aaa2db1ebf
hzhang <Haijun.Zhang@kisters.cn>
2016-03-08 13:30:19 +0800

    {nocase} databinding

--- js/main.js
+++ js/main.js
@@ -1,23 +1,61 @@
 var app = angular.module('myApp', []);
+app.run(function($rootScope) {
+  $rootScope.name = "Ari Lerner";
+});
+
+app.controller('MyController', function($scope) {
+  $scope.person = {
+    name: ""
+  };
+});
+
+app.controller('MyController', function($scope) {
+  $scope.person = { name: "Ari Lerner" };
+  var updateClock = function() {
+    $scope.time = new Date();
+  };
+  var timer = setInterval(function() {
+    $scope.$apply(updateClock);
+  }, 1000);
+  updateClock();
+});
+
+app.controller('DemoController', function($scope) {
+  $scope.counter = 0;
+  $scope.add = function(amount) { $scope.counter += amount; };
+  $scope.subtract = function(amount) { $scope.counter -= amount; };
+});
+
+var apiKey = 'MDIzMDk0NzE0MDE0NTc0MTQ0OTcwYzk3Mg000',
+    nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
  
-app.controller('PlayerController', ['$scope', function($scope) {
-  $scope.playing = false;
-  $scope.audio = document.createElement('audio');
-  $scope.audio.src = 'media/e.mp3';
-   $scope.play = function() {
-    $scope.audio.play();
-    $scope.playing = true;
-  };
-  $scope.stop = function() {
-    $scope.audio.pause();
-    $scope.playing = false;
-  };
-  $scope.audio.addEventListener('ended', function() {
-    $scope.$apply(function() {
-      $scope.stop()
-    });
+app.controller('PlayerController', function($scope, $http) {
+  
+// Hidden our previous section's content
+  
+// construct our http request
+  $http({
+    method: 'JSONP',
+    url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+  }).success(function(data, status) {
+    $scope.programs = data.list.story;
+// Now we have a list of the stories (data.list.story)
+    
+// in the data object that the NPR API 
+    
+// returns in JSON that looks like:
+    
+// data: { "list": {
+    
+//   "title": ...
+    
+//   "story": [
+    
+//     { "id": ...
+    
+//       "title": ...
+  }).error(function(data, status) {
+    
+// Some error occurred
   });
-}]);
- 
-app.controller('RelatedController', ['$scope', function($scope) {
-}]);
+});
